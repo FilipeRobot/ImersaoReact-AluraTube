589a2f9c03882a8e8198b33c8b4178089e1aee10
Filipe Lemos <filipe_lemos@live.com>
2022-11-09 16:33:45 -0300

    adicionando imagem ao banner

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ function Header() {
 		<StyledHeader>
 			<img
 				className="banner"
-				src="https://via.placeholder.com/150"
+				src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
 				alt="Banner"
 			/>
 			<section className="user-info">
